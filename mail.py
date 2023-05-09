0039178ba6435f2ee363d1931a79d6df4e7b98bf
ggWoop <wood1798@gmail.com>
2023-05-09 14:49:27 +0900

    커밋 2 곱하기 나누기 추가

--- mail.py
+++ mail.py
@@ -4,6 +4,11 @@ def add_func(n1 , n2):
 def sub_func(n1 , n2):
     return n1 - n2
 
+def x_func(n1 , n2):
+    return n1 * n2
+
+def divide_func(n1 , n2):
+    return n1 / n2
 
 num1,num2, res = 100, 200, 0
 
@@ -19,6 +24,18 @@ res = sub_func(num1,num2)
 print(f"{num1} - {num2} = {res}" )
 
 
+
+
+
+
+res = x_func(num1,num2)
+print(f"{num1} * {num2} = {res}" )
+
+
+
+
+res = divide_func(num1,num2)
+print(f"{num1} / {num2} = {res}" )
 
 
 
